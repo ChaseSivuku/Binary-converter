cf4d32e7ac5f3362029b34da8585b890fe43b139
ChaseSivuku <113906831+ChaseSivuku@users.noreply.github.com>
2025-08-05 21:44:18 +0200

    Add files via upload
    Added more functionalities on the convert.js file by writing more functions and refining the conversion-hub.html

--- convert.js
+++ convert.js
@@ -38,13 +38,63 @@ metricBtn.addEventListener("click", function(){
 //File Transfer Calculation
 function fileTransferTime(size, sizeUnit, intSpeed, intSpeedUnit){
     
+    let bytes = 0;
+    let bytesPS = 0;
+    
     switch(sizeUnit){
-        case "GB" : 
+        case "GB" : bytes = size * 1073741824;
+        break;
+        case "MB" : bytes = size * 1048576;
+        break;
+        case "KB" : bytes = size * 1024;
+        break;
+    }
+    switch(intSpeedUnit){
+        case "GBps" : bytesPS = intSpeed * 1073741824;
+        break;
+        case "MBps" : bytesPS = intSpeed * 1048576;
+        break;
+        case "KBps" : bytesPS = intSpeed * 1024;
+        break;
+    }
+
+    return Math.floor(bytes / bytesPS);
+}
+
+console.log(fileTransferTime(100, "MB", 5, "MBps"));
+
+function weatherConvert(value, unit){
+    if(unit === "f"){
+        return Math.floor(value - 32 * 5/9); 
+    } else if(unit === "c"){
+        return Math.floor(value * 9/5 + 32);
     }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
     hideAll();
     file.style.display = "flex";
+
+    const calcButton = document.getElementById("calc-button");
+    calcButton.addEventListener("click", function(){
+        const size = parseInt(document.getElementById("file-size").value);
+        const speed = parseInt(document.getElementById("internet-speed").value);
+        const speedUnit = document.getElementById("speed-unit").value;
+        const sizeUnit = document.getElementById("size-unit").value;
+
+        const answer = document.getElementById("time-result");
+        answer.textContent += fileTransferTime(size, sizeUnit, speed, speedUnit);
+    });
+    
+    const weatherButton = document.getElementById("convert-weather-button");
+    weatherButton.addEventListener("click", () =>{
+        const value = parseInt(document.getElementById("temp-value").value);
+        const unit = document.getElementById("temp-unit").value;
+
+        const result = document.getElementById("weather-converted");
+        result.textContent += weatherConvert(value, unit);
+    });
+
+    
 });
 
